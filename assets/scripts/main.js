c2ca19df1cf41c308f30282b6953fad4991a6859
shadmanrakib <srakibnyc1@gmail.com>
2025-03-11 23:48:29 -0400

    implement add row grid utility

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,3 +11,22 @@ function createCell() {
   return elem;
 }
 
+function updateGridLayout(rows, columns) {
+  /** @type {HTMLDivElement} */
+  const grid = document.querySelector(".grid");
+  grid.style = `--rows: ${rows}; --columns: ${columns}`;
+}
+
+function addRow() {
+  const grid = document.querySelector(".grid");
+  const style = window.getComputedStyle(grid);
+
+  const rows = Number.parseInt(style.getPropertyValue("--rows"));
+  const columns = Number.parseInt(style.getPropertyValue("--columns"));
+
+  for (let i = 0; i < columns; i++) {
+    grid.appendChild(createCell());
+  }
+
+  updateGridLayout(rows + 1, columns);
+}
