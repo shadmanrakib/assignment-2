636c3152bda17cb499b675442a1f7fac987cdbe0
shadmanrakib <srakibnyc1@gmail.com>
2025-03-11 23:46:39 -0400

    switch from body script to head script with DOMContentLoaded listener

--- assets/scripts/dropdown.js
+++ assets/scripts/dropdown.js
@@ -1,50 +1,52 @@
-/** @type {NodeListOf<HTMLElement>} */
-const dropdowns = document.querySelectorAll(".dropdown");
+document.addEventListener("DOMContentLoaded", () => {
+  /** @type {NodeListOf<HTMLElement>} */
+  const dropdowns = document.querySelectorAll(".dropdown");
 
-/**
- * @param {HTMLElement} dropdownElem
- */
-function closeDropdown(dropdownElem) {
-  dropdownElem.setAttribute("data-open", "false");
-}
-
-/**
- * @param {HTMLElement} dropdownElem
- */
-function openDropdown(dropdownElem) {
-  dropdownElem.setAttribute("data-open", "true");
-}
-
-/**
- * @param {HTMLElement} dropdownElem
- */
-function toggleDropdown(dropdownElem) {
-  const prevOpen = dropdownElem.getAttribute("data-open");
-  if (!prevOpen || prevOpen === "false") {
-    openDropdown(dropdownElem);
-  } else {
-    closeDropdown(dropdownElem);
+  /**
+   * @param {HTMLElement} dropdownElem
+   */
+  function closeDropdown(dropdownElem) {
+    dropdownElem.setAttribute("data-open", "false");
   }
-}
 
-dropdowns.forEach((dropdown) => {
-  // add toggling of menu
+  /**
+   * @param {HTMLElement} dropdownElem
+   */
+  function openDropdown(dropdownElem) {
+    dropdownElem.setAttribute("data-open", "true");
+  }
 
-  /** @type {NodeListOf<HTMLButtonElement>} */
-  const triggers = dropdown.querySelectorAll("button.dropdown-trigger");
-  triggers.forEach((trigger) => {
-    trigger.addEventListener("click", () => toggleDropdown(dropdown));
-  });
-
-  // if we click out or lose focus, we should close the dropdown
-  document.addEventListener("focusout", (e) => {
-    if (!dropdown.contains(e.relatedTarget)) {
-      closeDropdown(dropdown);
+  /**
+   * @param {HTMLElement} dropdownElem
+   */
+  function toggleDropdown(dropdownElem) {
+    const prevOpen = dropdownElem.getAttribute("data-open");
+    if (!prevOpen || prevOpen === "false") {
+      openDropdown(dropdownElem);
+    } else {
+      closeDropdown(dropdownElem);
     }
-  });
-  document.addEventListener("mousedown", (e) => {
-    if (!dropdown.contains(e.target)) {
-      closeDropdown(dropdown);
-    }
+  }
+
+  dropdowns.forEach((dropdown) => {
+    // add toggling of menu
+
+    /** @type {NodeListOf<HTMLButtonElement>} */
+    const triggers = dropdown.querySelectorAll("button.dropdown-trigger");
+    triggers.forEach((trigger) => {
+      trigger.addEventListener("click", () => toggleDropdown(dropdown));
+    });
+
+    // if we click out or lose focus, we should close the dropdown
+    document.addEventListener("focusout", (e) => {
+      if (!dropdown.contains(e.relatedTarget)) {
+        closeDropdown(dropdown);
+      }
+    });
+    document.addEventListener("mousedown", (e) => {
+      if (!dropdown.contains(e.target)) {
+        closeDropdown(dropdown);
+      }
+    });
   });
 });
