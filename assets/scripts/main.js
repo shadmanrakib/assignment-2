9a924c966d1092adead071537630d402bbe62c1d
Nazmul Dewan <nazmul.dewan58@myhunter.cuny.edu>
2025-03-15 17:32:59 -0400

    Added clear all colors

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -204,12 +204,10 @@ function colorAll() {
 }
 
 function clearAll() {
-  if (color !== -1) {
-    const cells = document.querySelectorAll('.cell');
+  const cells = document.querySelectorAll('.cell');
     
-    for (let i = 0; i < cells.length; i++) {
-      cells[i].style.backgroundColor = "white";  
-    }
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "white";  
   }
 }
 
